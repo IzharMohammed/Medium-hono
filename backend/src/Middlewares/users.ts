0b2234e884599c01bae12a5ad8f9f9f89e042706
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-08-10 13:23:51 +0530

    Added authentication and middlewares

---
+++ backend/src/Middlewares/users.ts
@@ -0,0 +1,6 @@
+const jwtPassword = 'secret';
+import {  verify } from 'hono/jwt';
+
+export default async function userMiddleware(c : any , next : any) {
+
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,28 +2,118 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { env } from 'hono/adapter'
-
+const jwtPassword = 'secret';
+import { decode, sign, verify } from 'hono/jwt'
 const app = new Hono()
+import { z as zod } from 'zod';
 
-app.get('/', (c) => {
+type responsePayload = {
+  email: string
+}
+
+app.use('/api/v1/blog/*', async (c, next) => {
+  const token = c.req.header('token');
+
+  if (!token) {
+    return c.json({
+      msg: 'Token needed',
+    });
+  }
+
+  try {
+    const response = await verify(token, jwtPassword) as responsePayload;
+    const email = response.email;
+
+    console.log(response);
+    // c.header('response', email);
+    c.set('jwtPayload', email)
+    await next();
+  } catch (error) {
+    return c.json({
+      msg: 'Invalid token',
+    }, 401);
+  }
+});
+
+
+
+app.post('/api/v1/user/signup', async (c) => {
+
   const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
-
   const prisma = new PrismaClient({
- datasourceUrl: DATABASE_URL,
-}).$extends(withAccelerate())
+    datasourceUrl: DATABASE_URL,
+  }).$extends(withAccelerate());
 
-  return c.text(DATABASE_URL)
+  const email = c.req.header('email');
+  const password = c.req.header('password');
+
+  const emailSchema = zod.string().email();
+  const passwordSchema = zod.string().min(4);
+
+  if (!email || !password) {
+    return c.json({
+      msg: 'Email and password are necessary'
+    });
+  }
+
+  const emailResponse = emailSchema.safeParse(email);
+  const passwordResponse = passwordSchema.safeParse(password);
+
+  if (!emailResponse.success) {
+    return c.json({
+      msg: 'Invalid email'
+    })
+  }
+
+  if (!passwordResponse.success) {
+    return c.json({
+      msg: 'Password must be minimum 4 characters'
+    })
+  }
+
+  const response = await prisma.user.create({
+    data: {
+      email,
+      password
+    }
+  })
+  console.log(response);
+
+  return c.text(`${email} logged in successfully !!! `)
 })
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('i am in !!!')
-})
+app.post('/api/v1/user/signin', async (c) => {
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('i am in 1 !!!')
+  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
+  const prisma = new PrismaClient({
+    datasourceUrl: DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const email = c.req.header('email');
+  const password = c.req.header('password');
+
+  if (!email || !password) {
+    return c.json({
+      msg: 'Email and password are necessary'
+    });
+  }
+
+  const response = await prisma.user.findFirst({
+    where: {
+      email
+    }
+  })
+
+  const token = await sign({ email }, jwtPassword);
+
+  console.log(response);
+
+  return c.text(token)
 })
 
 app.post('/api/v1/blog', (c) => {
+  console.log('response', c.get('jwtPayload'));
+
   return c.text('i am in 2 !!!')
 })
 
