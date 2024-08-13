8a81575c55b7a4f0b846ca23445036fef8c6c19a
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-08-13 19:55:50 +0530

    Added frontend pages

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,8 +1,25 @@
 
 function Signup() {
-  return (
-    <div>signup</div>
-  )
+    return (
+        <div className="border border-black w-[50rem] h-[20rem] mx-auto  flex flex-col justify-center items-center gap-3 ">
+            <div className="border border-black">
+            <h1>Welcome back</h1>
+            <p>Enter your email and password to create your account.</p>
+            </div>
+
+            <div className="border border-black">
+                <h2>Email</h2>
+                <input type="text" className="border outline-none" placeholder="m@example.com" />
+            </div>
+            <div>
+                <h2>Password</h2>
+                <input type="text" className="border outline-none " />
+            </div>
+            <div>
+                <button className="btn btn-success">Sign up</button>
+            </div>
+        </div>
+    )
 }
 
 export default Signup;
