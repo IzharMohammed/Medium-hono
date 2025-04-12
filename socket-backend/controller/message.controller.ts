e9392696bf1bcb7f0e7357fa3c56456168bea8d0
MohammedIzhar77 <izharmohammed21@gmail.com>
2025-04-12 18:15:07 +0530

    Added routes for message routes

---
+++ socket-backend/controller/message.controller.ts
@@ -0,0 +1,170 @@
+import { Request, Response } from "express"
+import { asyncHandler } from "../utils/asyncHandler"
+import { PrismaClient } from "@prisma/client";
+import { HttpStatusCode } from "../types";
+import { ApiResponse } from "../utils/apiResponse";
+import { emitSocketEvent } from "../socket";
+import { ChatEventEnum } from "../constants";
+import { ApiError } from "../utils/ApiError";
+
+const prisma = new PrismaClient();
+
+const getAllMessages = asyncHandler(async (req: Request, res: Response) => {
+
+    const { chatId } = req.params;
+    //@ts-ignore
+    const userId = req.user.id;
+
+    const selectedChat = await prisma.chat.findUnique({
+        where: {
+            id: Number(chatId)
+        },
+        include: {
+            participants: true,
+            messages: true,
+        }
+    });
+
+    if (!selectedChat) {
+        throw new ApiError(HttpStatusCode.NOT_FOUND, "Chat does not exist");
+    }
+
+    if (!selectedChat.participants.includes(userId)) {
+        throw new ApiError(HttpStatusCode.BAD_REQUEST, "user is not a part of this chat");
+    }
+
+    const messages = await prisma.chatMessage.findMany({
+        where: {
+            id: Number(chatId)
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+    });
+
+    res
+        .status(HttpStatusCode.OK)
+        .json(new ApiResponse(HttpStatusCode.OK, messages || [], "Messages fetched successfully...!!!"));
+});
+
+const sendMessage = asyncHandler(async (req: Request, res: Response) => {
+    const { chatId } = req.params;
+    const { content } = req.body;
+    //@ts-ignore
+    const userId = req.user.id;
+
+    if (!content) {
+        throw new ApiError(HttpStatusCode.BAD_REQUEST, "Message content or attachment is require");
+    }
+
+    const chats = await prisma.chat.findFirst({
+        where: {
+            id: Number(chatId),
+        },
+        include: {
+            participants: true,
+        }
+    });
+
+    if (!chats) {
+        throw new ApiError(HttpStatusCode.NOT_FOUND, "chats does not exist");
+    }
+
+    const message = await prisma.chatMessage.create({
+        data: {
+            content: content || "",
+            sender: {
+                connect: {
+                    id: Number(userId)
+                }
+            },
+            chat: {
+                connect: {
+                    id: Number(chatId),
+                }
+            }
+        },
+        include: {
+            sender: true,
+        }
+    });
+
+    const chat = await prisma.chat.update({
+        where: {
+            id: Number(chatId)
+        },
+        data: {
+            lastMessage: { connect: { id: message.id } }
+        },
+        include: {
+            participants: true
+        }
+    });
+
+    chat.participants.forEach(participant => {
+        if (participant.id !== Number(userId)) {
+            emitSocketEvent(
+                req,
+                participant.id.toString(),
+                ChatEventEnum.MESSAGE_RECEIVED_EVENT,
+                chat
+            );
+        }
+    });
+
+
+    res
+        .status(201)
+        .json(new ApiResponse(201, chat, "Message saved successfully"));
+});
+
+
+/* will finish it in future */
+// const deleteMessage = asyncHandler(async (req: Request, res: Response) => {
+//     const { chatId, messageId } = req.params;
+
+//     //@ts-ignore
+//     const userId = req.user.id;
+
+//     const chats = await prisma.chat.findFirst({
+//         where: {
+//             id: Number(chatId),
+//         },
+//         include: {
+//             participants: true,
+//         }
+//     });
+
+//     if (!chats) {
+//         throw new ApiError(HttpStatusCode.NOT_FOUND, "chats does not exist");
+//     }
+
+//     const message = await prisma.chat.findMany({
+//         where: {
+//             id: Number(messageId)
+//         },
+//         include: {
+//             participants: true,
+//             admin: true,
+
+//         }
+//     });
+
+//     if (!message) {
+//         throw new ApiError(404, "Message does not exist");
+//     }
+
+//     if (userId !==message.find(m=>m.)) {
+//         throw new ApiError(
+//             403,
+//             "You are not the authorised to delete the message, you are not the sender"
+//         );
+//     }
+
+// });
+
+export {
+    getAllMessages,
+    sendMessage,
+    // deleteMessage
+}
--- socket-backend/routes/message.routes.ts
+++ socket-backend/routes/message.routes.ts
@@ -1,6 +1,18 @@
 import { Router } from "express";
+import { verifyJwt } from "../middlewares/auth.middleware";
+import { getAllMessages, sendMessage } from "../controller/message.controller";
 
 const router = Router();
 
+router.use(verifyJwt);
+
+router
+    .route("/:chatId")
+    .get(getAllMessages)
+    .post(sendMessage);
+
+// router
+//     .route("/:chatId/:messageId")
+//     .delete(deleteMessage);
 
 export default router;
