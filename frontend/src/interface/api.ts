b507c09addfdd5a85dd0eb62901d593f00976500
MohammedIzhar77 <izharmohammed21@gmail.com>
2025-04-14 18:26:30 +0530

    Added utility classes

---
+++ frontend/src/interface/api.ts
@@ -0,0 +1,7 @@
+export interface FreeAPISuccessResponseInterface {
+    data: any;
+    message: string;
+    statusCode: number;
+    success: boolean;
+  }
+  
---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,75 @@
+import { AxiosResponse } from "axios";
+import { FreeAPISuccessResponseInterface } from "../interface/api";
+
+export const isBrowser = typeof window !== "undefined";
+
+// A utility function for handling API requests with loading, success, and error handling
+export const requestHandler = async (
+    api: () => Promise<AxiosResponse<FreeAPISuccessResponseInterface, any>>,
+    setLoading: ((loading: boolean) => void) | null,
+    onSuccess: (data: FreeAPISuccessResponseInterface) => void,
+    onError: (error: string) => void
+) => {
+    // Show loading state if setLoading function is provided
+    setLoading && setLoading(true);
+    try {
+        // Make the API request
+        const response = await api();
+        const { data } = response;
+        if (data?.success) {
+            // Call the onSuccess callback with the response data
+            onSuccess(data);
+        }
+    } catch (error: any) {
+        // Handle error cases, including unauthorized and forbidden cases
+        if ([401, 403].includes(error?.response.data?.statusCode)) {
+            localStorage.clear(); // Clear local storage on authentication issues
+            if (isBrowser) window.location.href = "/login"; // Redirect to login page
+        }
+        onError(error?.response?.data?.message || "Something went wrong");
+    } finally {
+        // Hide loading state if setLoading function is provided
+        setLoading && setLoading(false);
+    }
+};
+
+// A utility function to concatenate CSS class names with proper spacing
+export const className = (...classeName: string[]) => {
+    // Filter out any empty class names and join them with a space
+    return classeName.filter(Boolean).join(" ");
+}
+
+// A class that provides utility functions for working with local storage
+export class LocalStorage {
+    // Get a value from local storage by key
+    static get(key: string) {
+        if (!isBrowser) return;
+        const value = localStorage.getItem(key);
+        if (value) {
+            try {
+                return JSON.parse(value);
+            } catch (err) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    // Set a value in local storage by key
+    static set(key: string, value: any) {
+        if (!isBrowser) return;
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    // Remove a value from local storage by key
+    static remove(key: string) {
+        if (!isBrowser) return;
+        localStorage.removeItem(key);
+    }
+
+    // Clear all items from local storage
+    static clear() {
+        if (!isBrowser) return;
+        localStorage.clear();
+    }
+}
